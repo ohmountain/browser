9c7dc23a4c96098a902768dc72d135e2afe8d6c4
renshan <1005110700@qq.com>
2017-10-29 19:34:43 +0800

    完成区块详情

--- src/components/blocks.js
+++ src/components/blocks.js
@@ -11,7 +11,8 @@ class Blocks extends Component {
 
         this.state = {
             data: [],
-            timmer: null
+            timmer: null,
+            timmer1: null
         }
     };
 
@@ -22,9 +23,17 @@ class Blocks extends Component {
 
         this.randData();
 
-        setInterval(() => {
+        let timmer = setInterval(() => {
             this.randData();
         }, 10000);
+
+        this.setState({ timmer });
+    }
+
+    handleCloseBlockDetails() {
+        this.refs.block_detail.style.width="0px";
+        this.refs.block_detail.style.opacity=0;
+        this.refs.block_detail.style.transform = "translateX(480px)";
     }
 
     randData() {
@@ -45,23 +54,32 @@ class Blocks extends Component {
             });
         }
 
-        setTimeout(() => this.setState({ data }), 2000);
+        let timmer1 = setTimeout(() => this.setState({ data }), 2000);
+        this.setState({ timmer1 });
+    }
+
+    handleBlockItemClick() {
+        this.refs.block_detail.style.transform = "translateX(0px)";
+        this.refs.block_detail.style.opacity=1;
+        this.refs.block_detail.style.width="460px";
     }
 
     componentWillUnmount() {
+        clearInterval(this.state.timmer);
+        clearTimeout(this.state.timmer1);
         this.refs.container.classList.remove('container-mounted-position');
     }
 
     render() {
         let body = this.state.data.map((row, i) => {
             let time = `${ 1990 + row.time.getYear()}-${row.time.getMonth()+1}-${row.time.getDate()} ${row.time.getHours()}:${row.time.getMinutes()}:${row.time.getSeconds()}`;
-            return (<Link key={ row.id } to="/"><QueueAnim type="bottom" component="ul" className="horizontal-list">
+            return (<a onClick={ this.handleBlockItemClick.bind(this) } key={ row.id }><QueueAnim type="bottom" component="ul" className="horizontal-list">
                 <li key={row.id + 'height'} className="block-height-tb-text">{ row.height }</li>
                 <li key={row.id + 'merkle'}  className="block-height-tb-merkle">{ row.merkle }</li>
                 <li key={row.id + 'time'}  className="block-height-tb-time">{ time }</li>
                 <li key={row.id + 'transactions'}  className="block-height-tb-transactions">{ row.transactions }</li>
                 <li key={row.id + 'size'}  className="block-height-tb-size">{ row.size }</li>
-            </QueueAnim></Link>);
+            </QueueAnim></a>);
         });
 
         return (<div className="container" ref="container">
@@ -69,7 +87,8 @@ class Blocks extends Component {
                 minHeight: '720px',
                 width: '90%',
                 minWidth: '980px',
-                display:'block',
+                display:'flex',
+                flexDirection: 'column',
                 textAlign: 'left',
                 borderRadius: '4px',
                 padding: '24px',
@@ -77,31 +96,76 @@ class Blocks extends Component {
                 overflowX: 'hidden'}}>
 
                 <div className="block-text" style={{ fontSize: '24px' }}>区块</div>
-                <div className="block-table">
-                    <div className="block-tb-header" style={{ height: '48px' }}>
-                        <ul className="horizontal-list">
-                            <li className="block-height-tb-text">高度</li>
-                            <li className="block-height-tb-merkle">merkle</li>
-                            <li className="block-height-tb-time">时间</li>
-                            <li className="block-height-tb-transactions">交易数</li>
-                            <li className="block-height-tb-size">块大小</li>
-                        </ul>
-                    </div>
-                    <div className="block-tb-body">
-                        <QueueAnim type="top">
-                            <QueueAnim
-                                key="abc"
-                                type={['right', 'left']}
-                                ease={['easeOutQuart', 'easeInOutQuart']}>
-                                { body }
+
+                <div style={{
+                    display: 'flex',
+                    flexDirection: 'row'
+                }}>
+                    <div className="block-table" style={{flex:1}}>
+                        <div className="block-tb-header" style={{ height: '48px' }}>
+                            <ul className="horizontal-list">
+                                <li className="block-height-tb-text">高度</li>
+                                <li className="block-height-tb-merkle">merkle</li>
+                                <li className="block-height-tb-time">时间</li>
+                                <li className="block-height-tb-transactions">交易数</li>
+                                <li className="block-height-tb-size">块大小</li>
+                            </ul>
+                        </div>
+                        <div className="block-tb-body">
+                            <QueueAnim type="top">
+                                <QueueAnim
+                                    key="abc"
+                                    type={['right', 'left']}
+                                    ease={['easeOutQuart', 'easeInOutQuart']}>
+                                    { body }
+                                </QueueAnim>
                             </QueueAnim>
-                        </QueueAnim>
+                        </div>
+                    </div>
+                    <div style={{
+                        width: '0px',
+                        opacity: 0,
+                        marginLeft: '16px',
+                        marginTop: '16px',
+                        height: '380px',
+                        overflow: 'hidden',
+                        transitionProperty: 'width,opacity, transform',
+                        transitionDuration: '1s',
+                    }} ref="block_detail">
+                        <div className="block-detail-head">区块详情 <span onClick={ this.handleCloseBlockDetails.bind(this) } className="fa fa-close" style={{float:"right"}}></span></div>
+                        <div className="block-detail-body">
+                            <div className="block-detail-item">
+                                <div className="key"><div style={{ padding: "0 14px" }}>高度</div></div>
+                                <div className="value"><div style={{ padding: "0 14px" }}>192831231</div></div>
+                            </div>
+                            <div className="block-detail-item">
+                                <div className="key"><div style={{ padding: "0 14px" }}>出块时间</div></div>
+                                <div className="value"><div style={{ padding: "0 14px" }}>2017-11-21 19:00:00</div></div>
+                            </div>
+                            <div className="block-detail-item">
+                                <div className="key"><div style={{ padding: "0 14px" }}>区块Hash</div></div>
+                                <div className="value"><div style={{ padding: "0 14px" }}>b0498b62e12644d2d9a73d1aa44b8b25675d393b1d76d309dfae09069edb75ad</div></div>
+                            </div>
+                            <div className="block-detail-item">
+                                <div className="key"><div style={{ padding: "0 14px" }}>交易数</div></div>
+                                <div className="value"><div style={{ padding: "0 14px" }}>1231</div></div>
+                            </div>
+                            <div className="block-detail-item">
+                                <div className="key"><div style={{ padding: "0 14px" }}>块大小</div></div>
+                                <div className="value"><div style={{ padding: "0 14px" }}>245</div></div>
+                            </div>
+                            <div className="block-detail-item">
+                                <div className="key"><div style={{ padding: "0 14px" }}>区块Merkle</div></div>
+                                <div className="value"><div style={{ padding: "0 14px" }}>b0498b62e12644d2d9a73d1aa44b8b25675d393b1d76d309dfae09069edb75ad</div></div>
+                            </div>
+                       </div>
                     </div>
                 </div>
+
                 <div className="block-pagination">
                     <QueueAnim type="bottom">
-                        <ReactPaginate key="pagination" previousLabel={"上一页"}
-                                       nextLabel={"下一页"}
+                        <ReactPaginate key="pagination" previousLabel={"<"}
+                                       nextLabel={">"}
                                        breakClassName={"break-me"}
                                        initialPage={1}
                                        pageCount={ 20 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import CountUp from 'react-countup';
+import QueueAnim from 'rc-queue-anim';
 
 class Index extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-          blockHeight: 1990,
+          blockHeight: 199009112,
+          originHeight: 0,
           timmer: null
         };
     }
@@ -13,9 +16,10 @@ class Index extends Component {
     componentDidMount() {
         let timmer = setInterval(() => {
             this.setState({
+                originHeight: this.state.blockHeight ? this.state.blockHeight - 100: 0,
                 blockHeight: this.state.blockHeight + 1
             });
-        }, 1000);
+        }, 5000);
 
         this.setState({ timmer });
 
@@ -30,23 +34,84 @@ class Index extends Component {
     }
 
     render() {
-        return (<div className="container" ref="container">
-                    <div className="block-height-wrapper">
-                        <div>
-                            <span className="fa fa-fire fa-4x" style={{color: 'red', marginRight: '12px'}}></span>
-                            <span className="block-height-text">{ this.state.blockHeight }</span>
-                        </div>
-                        <div className="block-height-title">块高</div>
+        return (<div className="container" ref="container" style={{ flexDirection: 'column' }}>
+            <div style={{ width: '100%',display:'flex'}}>
+                <div className="block-height-wrapper">
+                    <div>
+                        <span className="fa fa-fire fa-4x" style={{color: 'red', marginRight: '12px'}}></span>
+                        <span className="block-height-text">
+                            <CountUp
+                                start={this.state.originHeight}
+                                end={this.state.blockHeight}
+                                useEasing={false}
+                                useGrouping={true}/>
+                        </span>
                     </div>
+                    <div className="block-height-title">块高</div>
+                </div>
 
-                    <div className="transaction-wrapper">
-                        <div>
-                            <span className="fa fa-bolt fa-4x" style={{color: 'red', marginRight: '12px'}}></span>
-                            <span className="transaction-number-text">199099</span>
-                        </div>
-                        <div className="transaction-title">交易量</div>
+                <div className="transaction-wrapper">
+                    <div>
+                        <span className="fa fa-bolt fa-4x" style={{color: 'red', marginRight: '12px'}}></span>
+                        <span className="transaction-number-text">
+                            <CountUp
+                                start={this.state.originHeight - 10002321}
+                                end={this.state.blockHeight - 10002321}
+                                useEasing={false}
+                                useGrouping={true}/>
+                        </span>
                     </div>
-                </div>);
+                    <div className="transaction-title">交易量</div>
+                </div>
+            </div>
+
+            <div className="nodes-wrapper">
+                    <QueueAnim className="nodes-list-table" type="bottom" animConfig={{ opacity:[0,1] }} delay={1000}>
+                        <div className="nodes-list-head">
+                            <ul className="horizontal-list">
+                                <li className="node-identifier">标识</li>
+                                <li className="node-type">类型</li>
+                                <li className="node-ip">IP</li>
+                                <li className="node-status">状态</li>
+                            </ul>
+                        </div>
+                        <div className="nodes-list-body">
+                            <ul className="horizontal-list" key={1}>
+                                <li className="node-identifier">node_01</li>
+                                <li className="node-type">类型1</li>
+                                <li className="node-ip">192.168.1.2</li>
+                                <li className="node-status">正常</li>
+                            </ul>
+
+                            <ul className="horizontal-list" key={2}>
+                                <li className="node-identifier">node_02</li>
+                                <li className="node-type">类型2</li>
+                                <li className="node-ip">192.168.1.2</li>
+                                <li className="node-status">正常</li>
+                            </ul>
+
+                            <ul className="horizontal-list" key={3}>
+                                <li className="node-identifier">node_03</li>
+                                <li className="node-type">类型1</li>
+                                <li className="node-ip">192.168.1.3</li>
+                                <li className="node-status">正常</li>
+                            </ul>
+                            <ul className="horizontal-list" key={4}>
+                                <li className="node-identifier">node_04</li>
+                                <li className="node-type">类型1</li>
+                                <li className="node-ip">192.168.1.4</li>
+                                <li className="node-status">正常</li>
+                            </ul>
+                            <ul className="horizontal-list" key={5}>
+                                <li className="node-identifier">node_5</li>
+                                <li className="node-type">类型2</li>
+                                <li className="node-ip">192.168.1.5</li>
+                                <li className="node-status">正常</li>
+                            </ul>
+                        </div>
+                    </QueueAnim>
+            </div>
+        </div>);
     }
 }
 
