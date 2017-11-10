173e6063d495e62e5ca8680274a3d1876887e0d2
renshan <1005110700@qq.com>
2017-11-10 16:11:04 +0800

    删除不必要的内容，修正地图显示

--- src/components/index.js
+++ src/components/index.js
@@ -22,140 +22,6 @@ class Index extends Component {
     }
 
     componentDidMount() {
-        setTimeout(() => {
-            this.refs.container.classList.add('container-mounted-position');
-
-            // 基于准备好的dom，初始化echarts实例
-            var typeChart = echarts.init(this.refs.nodeType);
-            var healthChart = echarts.init(this.refs.nodeHealth);
-
-            let option1 = {
-                tooltip : {
-                    trigger: 'item',
-                    formatter: "{a} <br/>{b} : {c} ({d}%)"
-                },
-                legend: {
-                    orient: 'vertical',
-                    left: 'right',
-                    data: ['服务节点','验证节点'],
-                    textStyle:{
-                        color:'#909090',
-                        fontSize:12
-                    }
-                },
-                color: ['#09f', '#039'],
-                series : [
-                    {
-                        name:'节点数量',
-                        type:'pie',
-                        radius: ['50%', '70%'],
-                        avoidLabelOverlap: false,
-                        label: {
-                            normal: {
-                                show: false,
-                                position: 'center'
-                            },
-                            emphasis: {
-                                show: true,
-                                textStyle: {
-                                    fontSize: '18',
-                                    fontWeight: 'bold'
-                                }
-                            }
-                        },
-                        labelLine: {
-                            normal: {
-                                show: false
-                            }
-                        },
-                        data:[
-                            {value:3, name:'服务节点'},
-                            {value:1, name:'验证节点'},
-                        ]
-                    }
-                ]
-            };
-
-            let option2 = {
-                tooltip : {
-                    trigger: 'item',
-                    formatter: "{a} <br/>{b} : {c} ({d}%)"
-                },
-                legend: {
-                    orient: 'vertical',
-                    left: 'right',
-                    data: ['正常节点','故障节点'],
-                    textStyle:{    //图例文字的样式
-                        color:'#909090',
-                        fontSize:12
-                    }
-                },
-                color: ['#09f', '#f22'],
-                series : [
-                    {
-                        name:'节点状态',
-                        type:'pie',
-                        radius: ['50%', '70%'],
-                        avoidLabelOverlap: false,
-                        label: {
-                            normal: {
-                                formatter: '{a|{a}}{abg|}\n{hr|}\n  {b|{b}：}{c}  {per|{d}%}  ',
-                                backgroundColor: '#eee',
-                                borderColor: '#aaa',
-                                borderWidth: 1,
-                                borderRadius: 4,
-                                rich: {
-                                    a: {
-                                        color: '#999',
-                                        lineHeight: 16,
-                                        align: 'center'
-                                    },
-                                    hr: {
-                                        borderColor: '#aaa',
-                                        width: '100%',
-                                        borderWidth: 0.5,
-                                        height: 0
-                                    },
-                                    b: {
-                                        fontSize: 12,
-                                        lineHeight: 24
-                                    },
-                                    per: {
-                                        color: '#eee',
-                                        backgroundColor: '#334455',
-                                        padding: [2, 4],
-                                        borderRadius: 2
-                                    }
-                                }
-                            },
-                            emphasis: {
-                                show: true,
-                                textStyle: {
-                                    fontSize: '14',
-                                    fontWeight: 'bold'
-                                }
-                            }
-                        },
-                        labelLine: {
-                            normal: {
-                                show: false
-                            }
-                        },
-                        data:[
-                            {value:3, name:'正常节点'},
-                            {value:1, name:'故障节点'},
-                        ]
-                    }
-                ]
-            };
-
-            setTimeout(() => {
-                typeChart.setOption(option1);
-                healthChart.setOption(option2);
-            }, 10);
-
-        }, 150);
-
 
         this.tm = setInterval(() => {
 
@@ -188,16 +54,16 @@ class Index extends Component {
 
         let points = [{
             x: 500,
-            y: 350
+            y: 260
         },{
-            x: 500,
-            y: 410
+            x: 476,
+            y: 326
         },{
-            x: 810,
-            y: 300
+            x: 820,
+            y: 220
         },{
-            x: 566,
-            y: 310
+            x: 560,
+            y: 238
         }];
 
         let nodePointers = this.state.nodes.map((n, i)=> {
@@ -301,29 +167,13 @@ class Index extends Component {
 
             </div>
 
-            <RcScrollAnim.Parallax animation={{ opacity: 1, scale:1, playScale: [0.8, 1] }}
-                                   style={{ transform: 'scale(0.9)', opacity: 0.9 }}>
-                <div className="nodes-type" style={{ displa:'flex', flexDirection:"column", zIndex: '99' }}>
-                    <div key="1" style={{ display: 'inline-block', width: '50%', verticalAlign: 'top'}}>
-                        <div ref="nodeType" style={{ height: '240px'}}></div>
-                    </div>
-                    <div key="2" style={{ display: 'inline-block', width: '50%', height: '100%'}}>
-                        <div ref="nodeHealth" style={{ height: '240px' }}></div>
-                    </div>
-                </div>
-            </RcScrollAnim.Parallax >
-
             <div className="general-info-wrapper">
                 <QueueAnim key="ssss" type={["right"]} duration={1250} className="info-wrapper">
-                    <div className="info-title">当前区块高度</div>
-                    <div key="1" className="info-number"><Flip number={ this.state.blockHeight } sep=',' /></div>
-                </QueueAnim>
-                <QueueAnim type="left" className="info-wrapper">
-                    <div className="info-title">节点数量</div>
-                    <div key="2" className="info-number">4</div>
+                    <div className="info-title">页面在线人数</div>
+                    <div key="1" className="info-number"><Flip number={ Math.ceil(Math.random() * 100) } sep=',' duration={500} /></div>
                 </QueueAnim>
                 <QueueAnim type="alpha" duration={2000} className="info-wrapper">
-                    <div className="info-title">交易数量</div>
+                    <div className="info-title">历史浏览量</div>
                     <div key="3" className="info-number"><Flip number={ this.state.transactions } sep=',' /></div>
                 </QueueAnim>
             </div>
